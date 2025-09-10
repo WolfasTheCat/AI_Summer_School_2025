729db473ec0ffe2f58bbde50cdb165dc0ba36364
Jakub Hrušovský <100232199+WolfasTheCat@users.noreply.github.com>
2025-09-10 22:40:32 +0200

    added task for robots

---
+++ Tasks/task4/Task_4_2.py
@@ -0,0 +1,17 @@
+import time
+from pyniryo import *
+
+IP = "169.254.200.200"      # change if needed
+WORKSPACE = "sleep"     # <-- set this to your workspace name from Niryo Studio
+SHAPE = ObjectShape.SQUARE  # choose: SQUARE or CIRCLE (or ANY)
+COLOR = ObjectColor.RED     # choose: RED, GREEN, BLUE (or ANY)
+HEIGHT_OFFSET = 0        # meters above computed pick pose (keep positive & safe)
+
+robot = NiryoRobot(IP)
+
+id = robot.set_conveyor()
+robot.run_conveyor(id,100,ConveyorDirection.FORWARD)
+while robot.digital_read(PinID.DI5) == PinState.HIGH:
+    pass
+robot.stop_conveyor(id)
+robot.close_connection()
